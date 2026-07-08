c59c896ccd606c92e449a35cea1785eeef8fcf68
Monica <monica.2301152@srec.ac.in>
2026-07-08 15:23:20 +0530

    add react/session1 Section 1 — First Component

--- react/session1/src/App.tsx
+++ react/session1/src/App.tsx
@@ -1,14 +1,25 @@
 import './App.css'
+import Greeting from './Greeting'
 
 function App() {
   return (
-    <div>
+    <>
       <h1>Hello React</h1>
-      <p>My first React component</p>
-    </div>
+      <Greeting />
+    </>
   )
 }
 
 export default App
 
+// Task 1.1
+
 // A React component is used to create and display UI.
+
+// Task 1.2
+
+// A React component is a function that shows something written inside the function on the webpage.
+
+// Task 1.3
+
+// Fragment helps to avoid an extra div in the React DOM.
